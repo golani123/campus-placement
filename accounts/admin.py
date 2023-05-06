a3b14a615d4376520868066c8f4963a1e2ecc5f5
golani123 <108215443+golani123@users.noreply.github.com>
2023-05-06 16:04:10 +0530

    admin panel changes

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from accounts.models import *
+
+admin.site.register(User)
 
 # Register your models here.
--- jobsapp/admin.py
+++ jobsapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from jobsapp.models import *
 
+admin.site.register(Job)
+admin.site.register(Applicant)
 # Register your models here.
